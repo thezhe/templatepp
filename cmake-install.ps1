145cdec87f360d26dceb4305deefee21fc65abd7
Zhe Deng <42720670+thezhe@users.noreply.github.com>
2023-12-25 19:43:01 -0500

    no Ninja
    Signed-off-by: Zhe Deng <42720670+thezhe@users.noreply.github.com>

--- cmake-install.ps1
+++ cmake-install.ps1
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-pip install Ninja
-cmake -B ./build -DCMAKE_INSTALL_PREFIX=./build/install -DCMAKE_BUILD_TYPE=Release -G Ninja
+cmake -B ./build -DCMAKE_INSTALL_PREFIX=./build/install -DCMAKE_BUILD_TYPE=Release
 cmake --build ./build
 ctest --test-dir ./build
 cmake --install ./build --strip

6788e101c2d9325a1bd374638f18cd3519c358f2
Zhe Deng <42720670+thezhe@users.noreply.github.com>
2024-01-20 23:26:47 +0000

    fix CI_BUILD_VERSION
    Signed-off-by: Zhe Deng <42720670+thezhe@users.noreply.github.com>

--- wrun.cmake
+++ wrun.cmake
@@ -1,30 +1,25 @@
 #[[
-Run a url as a CMake script.
+Run a url as a CMake script
 
     cmake -P <path-to-wrun.cmake> <url>
 
-Fatal if: <url> content fails.
+Fatal if: <url> content fails
 Dependencies: curl
 ]]
 
-# See `execute_process` COMMAND_ERROR_IS_FATAL.
+# See `COMMAND_ERROR_IS_FATAL` in `execute_process`
 cmake_minimum_required(VERSION 3.19 FATAL_ERROR)
-
-# Check dependencies.
+# Dependencies
 execute_process(COMMAND curl --version OUTPUT_QUIET COMMAND_ERROR_IS_FATAL ANY)
-
 # CLI
-set(WRUN_URL "${CMAKE_ARGV3}")
-set(WRUN_ARGC 4)
-if(NOT WRUN_URL OR ${CMAKE_ARGC} GREATER ${WRUN_ARGC})
+math(EXPR argc_m_1 "${CMAKE_ARGC} - 1")
+set(wrun_url "${CMAKE_ARGV${argc_m_1}}")
+if(NOT wrun_url)
     message(FATAL_ERROR "Usage: cmake -P <path-to-wrun.cmake> <url>")
 endif()
-
-# Store <url> content in CURL_OUTPUT.
+# Store <url> content in `curl_output`
 execute_process(
-    COMMAND curl -fsSL "${WRUN_URL}"
-    OUTPUT_VARIABLE CURL_OUTPUT ECHO_ERROR_VARIABLE COMMAND_ERROR_IS_FATAL ANY
-)
-
-# Run <url> contents.
-cmake_language(EVAL CODE "${CURL_OUTPUT}")
+    COMMAND curl -fsSL "${wrun_url}"
+    OUTPUT_VARIABLE curl_output ECHO_ERROR_VARIABLE COMMAND_ERROR_IS_FATAL ANY)
+# Run `curl_output`
+cmake_language(EVAL CODE "${curl_output}")
